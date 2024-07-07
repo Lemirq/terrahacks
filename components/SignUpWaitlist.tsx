6ce7a6e03a431fe2f37f10c4b25fd4bdce656910
Lemirq <sharmavihaan190@gmail.com>
2024-07-06 21:20:30 -0400

    Text sizing fixed, Aurora light mode fixed

--- components/SignUpWaitlist.tsx
+++ components/SignUpWaitlist.tsx
@@ -32,7 +32,7 @@ const SignUpWaitlist = () => {
 
 	return (
 		<div className="max-w-3xl mx-auto mt-14 z-50">
-			<motion.h1 className="relative z-10 text-6xl md:text-9xl bg-clip-text text-transparent bg-gradient-to-b from-blue-300 to-blue-900 text-center font-sans font-bold">
+			<motion.h1 className="relative z-10 text-6xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-blue-300 to-blue-900 text-center font-sans font-bold">
 				Hack49
 			</motion.h1>
 			<h3 className="text-neutral-300 mx-auto my-2 text-2xl text-center relative z-10 mb-3 font-bold">2024</h3>
--- components/aurora.tsx
+++ components/aurora.tsx
@@ -27,8 +27,9 @@ export const AuroraBackground = ({ className, children, showRadialGradient = tru
             [background-image:var(--dark-gradient),var(--aurora)]
             [background-size:300%,_200%]
             [background-position:50%_50%,50%_50%]
-            filter blur-[10px] invert-0
-            after:content-[""] after:absolute after:inset-0 after:[background-image:var(--dark-gradient),var(--aurora)] 
+            filter blur-[10px] invert dark:invert-0
+            after:content-[""] after:absolute after:inset-0
+            after:[background-image:var(--dark-gradient),var(--aurora)]
             after:[background-size:200%,_100%] 
             after:animate-aurora after:[background-attachment:fixed] after:mix-blend-difference
             pointer-events-none
