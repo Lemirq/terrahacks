4065310646aaf2b49c297ded917b23d612813137
Lemirq <sharmavihaan190@gmail.com>
2024-08-18 16:18:59 -0400

    Updated prize tiers to only include shirt, added generate image button that appears after application is complete on the dashboard

--- app/dashboard/PrizeTiers.tsx
+++ app/dashboard/PrizeTiers.tsx
@@ -1,118 +1,137 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { IoClose } from 'react-icons/io5';
-import { Button } from '@nextui-org/react';
-const PrizeTiers = ({ setVisibility }: { setVisibility: (value: boolean) => void }) => {
-	{
-		/* 5 ppl: 1 ticket on winning t-shirt
+import React from "react";
+import { motion } from "framer-motion";
+import { IoClose } from "react-icons/io5";
+import { Button } from "@nextui-org/react";
+import Image from "next/image";
+const PrizeTiers = ({
+  setVisibility,
+}: {
+  setVisibility: (value: boolean) => void;
+}) => {
+  {
+    /* 5 ppl: 1 ticket on winning t-shirt
 
 10 ppl: 2 tickets on winning t-shirt
 
-20 ppl: 3 tickets on winning t-shirt + stickers
+20 ppl: 3 tickets on winning t-shirtrs
 
-50 ppl: 4 tickets on winning t-shirt + hat
+50 ppl: 4 tickets on winning t-shirtat
 
-75 ppl: 5 tickets on winning t-shirt + water bottles
+75 ppl: 5 tickets on winning t-shirtes
 
 1mil referrals = Guaranteed tesla with mockup */
-	}
-	const prizes = [
-		{
-			tickets: 5,
-			prize: '1 entry on winning Hack49 t-shirt',
-		},
-		{
-			tickets: 10,
-			prize: '2 entries on winning Hack49 t-shirt',
-		},
-		{
-			tickets: 20,
-			prize: '3 entries on winning Hack49 t-shirt + custom stickers',
-		},
-		{
-			tickets: 50,
-			prize: '4 entries on winning Hack49 t-shirt + custom hat',
-		},
-		{
-			tickets: 75,
-			prize: '5 entries on winning Hack49 t-shirt + custom water bottle',
-		},
-		{
-			tickets: '1 million',
-			prize: 'Guaranteed Tesla with Hack49 logo',
-		},
-	];
+  }
+  const prizes = [
+    {
+      referrals: 5,
+      prize: 1,
+    },
+    {
+      referrals: 10,
+      prize: 2,
+    },
+    {
+      referrals: 20,
+      prize: 3,
+    },
+    {
+      referrals: 50,
+      prize: 4,
+    },
+    {
+      referrals: 75,
+      prize: 5,
+    },
+  ];
 
-	const products = [
-		{
-			name: 'Hack49 T-shirt',
-			image: 'https://png.pngtree.com/png-clipart/20230520/ourmid/pngtree-realistic-plain-black-t-shirt-mockup-png-image_7103580.png',
-		},
-		{
-			name: 'Hack49 Hat',
-			image: 'https://static.vecteezy.com/system/resources/previews/026/860/472/original/cap-mockup-isolated-on-transparent-background-realistic-cap-hat-templates-angle-view-of-adult-caps-mockup-generative-ai-png.png',
-		},
-		{
-			name: 'Hack49 Water Bottle',
-			image: 'https://static.vecteezy.com/system/resources/previews/024/952/476/original/white-metal-water-bottle-3d-render-isolated-white-metallic-bottle-mockup-png.png',
-		},
-		{
-			name: 'Hack49 Stickers',
-			image: 'https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA2L3JtMzYzLWIwNS5qcGc.jpg',
-		},
-		{
-			name: 'Tesla Model 3',
-			className: 'col-span-2 h-96',
-			image: 'https://www.greenncap.com/wp-content/uploads/tesla-model-3-2022-0099.png',
-		},
-	];
+  // const products = [
+  // 	{
+  // 		name: 'Hack49 T-shirt',
+  // 		image: 'https://png.pngtree.com/png-clipart/20230520/ourmid/pngtree-realistic-plain-black-t-shirt-mockup-png-image_7103580.png',
+  // 	},
+  // 	{
+  // 		name: 'Hack49 Hat',
+  // 		image: 'https://static.vecteezy.com/system/resources/previews/026/860/472/original/cap-mockup-isolated-on-transparent-background-realistic-cap-hat-templates-angle-view-of-adult-caps-mockup-generative-ai-png.png',
+  // 	},
+  // 	{
+  // 		name: 'Hack49 Water Bottle',
+  // 		image: 'https://static.vecteezy.com/system/resources/previews/024/952/476/original/white-metal-water-bottle-3d-render-isolated-white-metallic-bottle-mockup-png.png',
+  // 	},
+  // 	{
+  // 		name: 'Hack49 Stickers',
+  // 		image: 'https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA2L3JtMzYzLWIwNS5qcGc.jpg',
+  // 	},
+  // 	{
+  // 		name: 'Tesla Model 3',
+  // 		className: 'col-span-2 h-96',
+  // 		image: 'https://www.greenncap.com/wp-content/uploads/tesla-model-3-2022-0099.png',
+  // 	},
+  // ];
 
-	return (
-		<div className="fixed w-screen h-screen overflow-x-hidden z-50 inset-0 justify-start fc overflow-y-scroll">
-			<motion.div
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				exit={{ opacity: 0 }}
-				onClick={() => setVisibility(false)}
-				className="bg-black/50 w-full h-full absolute top-0 left-0 z-0"
-			/>
+  return (
+    <div className="fixed w-screen h-screen overflow-x-hidden z-50 inset-0 justify-start fc overflow-y-scroll">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        onClick={() => setVisibility(false)}
+        className="bg-black/50 w-full h-full absolute top-0 left-0 z-0"
+      />
 
-			{/* prizes */}
-			<motion.div
-				initial={{ scale: 1.2, opacity: 0 }}
-				animate={{ scale: 1, opacity: 1 }}
-				exit={{ scale: 0.8, opacity: 0 }}
-				transition={{ duration: 0.2 }}
-				className="rainbow-shadow relative w-full max-w-6xl my-10"
-			>
-				{/* close button */}
-				<Button isIconOnly onClick={() => setVisibility(false)} className="absolute top-4 right-4 z-20 tex2xl">
-					<IoClose />
-				</Button>
-				<div className="fc z-10 w-full bg-neutral-900/90 relative p-10 backdrop-blur-2xl rounded-2xl">
-					<div className="fc sm:fr sm:gap-10 sm:justify-between">
-						<div className="fc gap-2">
-							<h1 className="text-6xl mb-4 w-full">Referral Prizes</h1>
-							<ul className="prizes-ul w-full fc items-start">
-								{prizes.map((item, index) => (
-									<li key={index} className="my-4 text-xl">
-										{item.tickets} tickets: {item.prize}
-									</li>
-								))}
-							</ul>
-						</div>
-						<div className="grid grid-cols-2 w-full h-full gap-4">
+      {/* prizes */}
+      <motion.div
+        initial={{ scale: 1.2, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{ scale: 0.8, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        className="rainbow-shadow relative w-full max-w-4xl my-10"
+      >
+        {/* close button */}
+        <Button
+          isIconOnly
+          onClick={() => setVisibility(false)}
+          className="absolute top-4 right-4 z-20 tex2xl"
+        >
+          <IoClose />
+        </Button>
+        <div className="fc z-10 w-full bg-neutral-900/90 relative p-10 backdrop-blur-2xl rounded-2xl">
+          <div className="fc sm:fr sm:gap-10 sm:justify-between w-full">
+            <div className="fc gap-2 w-full">
+              <h1 className="text-4xl mb-4 w-full">Referral Prizes</h1>
+              <ul className="prizes-ul w-full fc items-start">
+                {prizes.map((item, index) => (
+                  <li key={index} className="my-1 text-xl">
+                    {item.referrals} referrals: {item.prize}{" "}
+                    {item.prize > 1 ? "tickets" : "ticket"}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="fc gap-2 w-full">
+              <Image
+                src="/images/TSHIRT.png"
+                width={400}
+                height={200}
+                className="w-full max-w-[400px]"
+                alt="tshirt"
+              />
+              <p>
+                T-shirt may not be exactly as shown as we are still finalizing
+                the design.
+              </p>
+            </div>
+            {/* <div className="grid grid-cols-2 w-full h-full gap-4">
 							{products.map((product, index) => (
 								<div key={index} className={`size-48 w-full p-2 rounded-lg bg-neutral-300 ${product.className || 'col-span-1'}`}>
 									<img className="h-full w-full object-contain" src={product.image} alt="prize" />
 								</div>
 							))}
-						</div>
-					</div>
-				</div>
-			</motion.div>
-		</div>
-	);
+						</div> */}
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default PrizeTiers;
--- app/dashboard/Referral.tsx
+++ app/dashboard/Referral.tsx
@@ -1,131 +1,153 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { User } from '@/node_modules/@supabase/auth-js/src/lib/types';
-import { Button, Input } from '@nextui-org/react';
-import { createClient } from '@/utils/supabase/client';
-import { toast } from 'sonner';
-import { Tables } from '@/database.types';
-import { IoTicket } from 'react-icons/io5';
-import PrizeTiers from './PrizeTiers';
-import { AnimatePresence } from 'framer-motion';
-const prizes = [
-	{
-		tickets: 5,
-		prize: '1 entry on winning Hack49 t-shirt',
-	},
-	{
-		tickets: 10,
-		prize: '2 entries on winning Hack49 t-shirt',
-	},
-	{
-		tickets: 20,
-		prize: '3 entries on winning Hack49 t-shirt + custom stickers',
-	},
-	{
-		tickets: 50,
-		prize: '4 entries on winning Hack49 t-shirt + custom hat',
-	},
-	{
-		tickets: 75,
-		prize: '5 entries on winning Hack49 t-shirt + custom water bottle',
-	},
-	{
-		tickets: '1 million',
-		prize: 'Guaranteed Tesla with Hack49 logo',
-	},
-];
-const Referral = ({ user, refCode }: { user: User; refCode: Tables<'referrals'>[] }) => {
-	const [referralCode, setReferralCode] = useState<string>('');
-	const [created, setCreated] = useState<boolean>(refCode.length > 0);
-	const [code, setCode] = useState<Tables<'referrals'>[]>(refCode);
+"use client";
+import React, { useEffect, useState } from "react";
+import { User } from "@/node_modules/@supabase/auth-js/src/lib/types";
+import { Button, Input } from "@nextui-org/react";
+import { createClient } from "@/utils/supabase/client";
+import { toast } from "sonner";
+import { Tables } from "@/database.types";
+import { IoTicket } from "react-icons/io5";
+import PrizeTiers from "./PrizeTiers";
+import { AnimatePresence } from "framer-motion";
+import Link from "next/link";
 
-	const [prizesVisible, setPrizesVisible] = useState(false);
+const Referral = ({
+  user,
+  refCode,
+  app,
+}: {
+  user: User;
+  app: Tables<"applications">;
+  refCode: Tables<"referrals">[];
+}) => {
+  const [referralCode, setReferralCode] = useState<string>("");
+  const [created, setCreated] = useState<boolean>(refCode.length > 0);
+  const [code, setCode] = useState<Tables<"referrals">[]>(refCode);
 
-	const supabase = createClient();
-	const fetchUsedReferrals = async () => {
-		const { data, error } = await supabase.from('referrals').select('*').eq('user_id', user.id);
-		if (error) {
-			console.error(error.message);
-		}
-		if (data) {
-			setCode(data);
-			setCreated(true);
-		}
-	};
-	const createReferralCode = async () => {
-		// create referral code
-		const { error } = await supabase.from('referrals').insert({ user_id: user.id, code: referralCode });
-		if (error) {
-			if (error.code === '23505') {
-				toast.error('Referral code already exists, pick a different one');
-				return;
-			}
-		}
+  const [prizesVisible, setPrizesVisible] = useState(false);
 
-		setCreated(true);
-		toast.success('Referral code created successfully');
-		fetchUsedReferrals();
-	};
+  const supabase = createClient();
+  const fetchUsedReferrals = async () => {
+    const { data, error } = await supabase
+      .from("referrals")
+      .select("*")
+      .eq("user_id", user.id);
+    if (error) {
+      console.error(error.message);
+    }
+    if (data) {
+      setCode(data);
+      setCreated(true);
+    }
+  };
+  const createReferralCode = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (referralCode.length < 3) {
+      toast.error("Referral code must be at least 3 characters long");
+      return;
+    }
 
-	useEffect(() => {
-		console.log('refCode', code);
-	}, [code]);
+    // create referral code
+    const { error } = await supabase
+      .from("referrals")
+      .insert({ user_id: user.id, code: referralCode });
+    if (error) {
+      if (error.code === "23505") {
+        toast.error("Referral code already exists, pick a different one");
+        return;
+      }
+    }
 
-	// useEffect(() => {
-	// 	fetchUsedReferrals();
-	// }, []);
+    setCreated(true);
+    toast.success("Referral code created successfully");
+    fetchUsedReferrals();
+  };
 
-	const setVisibility = (value: boolean) => {
-		setPrizesVisible(value);
-	};
+  useEffect(() => {
+    console.log("refCode", code);
+  }, [code]);
 
-	return (
-		<section className="w-full rainbow-shadow relative mt-5">
-			<AnimatePresence>{prizesVisible && <PrizeTiers setVisibility={setVisibility} />}</AnimatePresence>
-			<div className="rounded-2xl border py-5 px-5 sm:px-10 border-neutral-300/30 w-full bg-neutral-900  relative z-10 fc items-start md:fr md:justify-between gap-10 justify-between">
-				<div className="z-10 fc items-start">
-					<h3 className="text-3xl font-bold">Referral Raffle and Prizes</h3>
-					<p className="text-lg">Refer a friend to get a chance to win a prize!</p>
-					<p>
-						After creating a referral code, share it with your friends. When they <b>submit an application</b> using your code, you get a
-						ticket.
-					</p>
+  // useEffect(() => {
+  // 	fetchUsedReferrals();
+  // }, []);
 
-					{created && code.length > 0 && (
-						<button
-							className="my-3"
-							onClick={() => {
-								navigator.clipboard.writeText(code[0].code);
-								toast.success('Code copied to clipboard');
-							}}
-						>
-							Your code: <span className="font-bold">{code[0].code}</span>
-						</button>
-					)}
-					<Button className="mt-3" onClick={() => setPrizesVisible(true)} color="primary">
-						View Prize Tiers
-					</Button>
-				</div>
-				<div className="fc gap-2 z-10 w-full">
-					<h3 className="text-2xl font-bold">{created ? 'Referral Tickets' : 'Create a referral code'}</h3>
-					{!created && (
-						<div className="fr gap-2 w-full">
-							<Input placeholder="Create referral code..." value={referralCode} onChange={(e) => setReferralCode(e.target.value)} />
-							<Button isDisabled={!referralCode} onClick={createReferralCode}>
-								Create
-							</Button>
-						</div>
-					)}
-					{created && code[0] && (
-						<div className="text-7xl font-bold tracking-tighter fr">
-							<IoTicket className="mr-2 text-4xl" />
-							{code[0].used}
-						</div>
-					)}
-				</div>
-			</div>
-		</section>
-	);
+  const setVisibility = (value: boolean) => {
+    setPrizesVisible(value);
+  };
+
+  return (
+    <section className="w-full rainbow-shadow relative mt-5">
+      <AnimatePresence>
+        {prizesVisible && <PrizeTiers setVisibility={setVisibility} />}
+      </AnimatePresence>
+      <div className="rounded-2xl border py-5 px-5 sm:px-10 border-neutral-300/30 w-full bg-neutral-900  relative z-10 fc items-start md:fr md:justify-between gap-10 justify-between">
+        <div className="z-10 fc items-start">
+          <h3 className="text-3xl font-bold">Referral Raffle</h3>
+          <p className="text-lg">
+            Refer a friend to get a chance to win a Hack49 t-shirt!
+          </p>
+          <p>
+            After creating a referral code, share it with your friends. When
+            they <b>submit an application</b> using your code, you get a ticket
+            for the raffle.
+          </p>
+
+          {created && code.length > 0 && (
+            <button
+              className="my-3"
+              onClick={() => {
+                navigator.clipboard.writeText(code[0].code);
+                toast.success("Code copied to clipboard");
+              }}
+            >
+              Your code: <span className="font-bold">{code[0].code}</span>
+            </button>
+          )}
+          <div className="fr gap-2 mt-4">
+            {" "}
+            <Button onClick={() => setPrizesVisible(true)} color="primary">
+              View Tiers
+            </Button>
+            {app.complete && (
+              <Link
+                href={
+                  created
+                    ? `/api/generate_img?code=${code[0].code}&mode=applied`
+                    : "/api/generate_img?mode=applied"
+                }
+                target="_blank"
+              >
+                <Button color="primary">Generate Image</Button>
+              </Link>
+            )}
+          </div>
+        </div>
+        <div className="fc gap-2 z-10 w-full">
+          <h3 className="text-2xl font-bold">
+            {created ? "Referral Tickets" : "Create a referral code"}
+          </h3>
+          {!created && (
+            <form className="fr gap-2 w-full" onSubmit={createReferralCode}>
+              <Input
+                maxLength={10}
+                placeholder="Create referral code..."
+                value={referralCode}
+                onChange={(e) => setReferralCode(e.target.value)}
+              />
+              <Button isDisabled={!referralCode} type="submit">
+                Create
+              </Button>
+            </form>
+          )}
+          {created && code[0] && (
+            <div className="text-7xl font-bold tracking-tighter fr">
+              <IoTicket className="mr-2 text-4xl" />
+              {code[0].used}
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Referral;
