8c5351f59adbcdfba80c59fb5a47d15862da7b7f
Lemirq <sharmavihaan190@gmail.com>
2024-08-25 13:59:25 -0400

    Added resend automatic audience addition

---
+++ app/api/delete_audience/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
+
+export async function GET() {
+	return NextResponse.json({ authorized: false });
+	const resend = new Resend(process.env.RESEND);
+
+	const { data } = await resend.contacts.list({
+		audienceId: '846181cc-3b35-47d6-a6ee-f69a68d761d4',
+	});
+	console.log('contacts', data);
+	if (!data) {
+		return NextResponse.json({ success: true });
+	}
+
+	for (let i = 0; i < data.data.length; i++) {
+		const contact = data.data[i];
+		const { data: c, error } = await resend.contacts.remove({
+			id: contact.id,
+			audienceId: '846181cc-3b35-47d6-a6ee-f69a68d761d4',
+		});
+		console.log('contact removed', c);
+		if (error) {
+			console.log('error', error);
+		}
+		await new Promise((resolve) => setTimeout(resolve, 500));
+	}
+	return NextResponse.json({ success: true });
+}
+
+export const fetchCache = 'force-no-store';
+export const revalidate = 0;
--- main.py
+++
@@ -1,8 +0,0 @@
-# in this dir, el.csv exists, has columns: id,name,email,created_at. Your job is to split the name into first_name and last_name, and save the result to a new file called el_new.csv
-# last name may or exist
-import pandas as pd
-
-df = pd.read_csv('el.csv')
-df['first_name'] = df['name'].apply(lambda x: x.split()[0])
-df['last_name'] = df['name'].apply(lambda x: ' '.join(x.split()[1:]) if len(x.split()) > 1 else None)
-df.to_csv('el_new.csv', index=False)
